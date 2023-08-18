17e56a004259c52c560ca461678d13c4c1e4c7aa
Anderson <anderson.fndz@gmail.com cnfig --global user.name Your>
2023-08-17 23:41:34 -0300

    ajust

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 var container = document.querySelector('.container')
 
+function insert(num){
+    var value = num
+    var texto = document.querySelector('#text')
 
+    texto.innerHTML = `You selected ${value} out of 5`
+}
+
+function clickBtn(){
+    container.style.display = 'none'
+}
