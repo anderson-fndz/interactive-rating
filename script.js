341dff8ac5397a5070a5c236a5d4624437588e8a
Anderson <anderson.fndz@gmail.com cnfig --global user.name Your>
2023-08-19 18:40:36 -0300

    project-complete

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var container = document.querySelector('.container')
+/*var container = document.querySelector('.container')
 
 function insert(num){
     var value = num
@@ -9,4 +9,46 @@ function insert(num){
 
 function clickBtn(){
     container.style.display = 'none'
+}*/
+let containerDisplay = document.querySelector('.container')
+let textSelected = document.querySelector('#text')
+let form = document.querySelector('#form')
+let modal = document.querySelector('.modal')
+
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    rating = form.inputradio
+
+    let value
+    let falso = 0
+
+    for (let index = 0; index < rating.length; index++) {
+        if (rating[index].checked === true) {
+            value = rating[index].value
+            containerDisplay.style.display = 'none'
+            textSelected.innerHTML = `You selected ${value} out of 5`
+            break
+        } else {
+            falso = falso + 1
+        }
+    }
+
+    if(falso === 5){
+        modal.style.display = 'flex'
+    }
+    /*
+    rating.forEach(input => {
+    
+            let value
+    
+            if (input.checked === true) {
+                value = input.value
+                containerDisplay.style.display = 'none'
+                textSelected.innerHTML = `You selected ${value} out of 5`
+            }
+        });*/
+})
+
+function closeModal(){
+    modal.style.display = 'none'
 }
